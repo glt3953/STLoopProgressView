4c138e08c1cfe5009d3369ddcafba8dbb712ec62
guoliting <guoliting@didichuxing.com>
2016-07-26 15:06:54 +0800

    增加波纹的遮罩

--- STLoopProgressView/STLoopProgressView/STLoopProgressView.m
+++ STLoopProgressView/STLoopProgressView/STLoopProgressView.m
@@ -17,6 +17,18 @@
 @property (strong, nonatomic) CAShapeLayer *colorMaskLayer; // 渐变色遮罩
 @property (strong, nonatomic) CAShapeLayer *colorLayer; // 渐变色
 @property (strong, nonatomic) CAShapeLayer *blueMaskLayer; // 蓝色背景遮罩
+@property (strong, nonatomic) CAShapeLayer *waveLineLayer; // 波纹线遮罩
+@property (nonatomic) NSUInteger numberOfWaves;
+@property (nonatomic) CGFloat amplitude; //振幅
+@property (nonatomic) CGFloat waveWidth;
+@property (nonatomic) CGFloat density; //密度
+@property (nonatomic) CGFloat waveMid;
+@property (nonatomic) CGFloat maxAmplitude;
+@property (nonatomic) CGFloat phase;
+@property (nonatomic) CGFloat waveHeight;
+@property (nonatomic) CGFloat phaseShift; //相移
+@property (nonatomic) CGFloat idleAmplitude;
+@property (nonatomic) CGFloat frequency;
 
 @end
 
@@ -29,15 +41,61 @@
     self.backgroundColor = [STLoopProgressView backgroundColor];
     
     [self setupColorLayer];
-    [self setupColorMaskLayer];
-    [self setupBlueMaskLayer];
+//    [self setupColorMaskLayer];
+//    [self setupBlueMaskLayer];
+    [self setupWaveLineLayer];
+}
+
+/**
+ *  设置波纹view的遮罩
+ */
+- (void)setupWaveLineLayer {
+    _numberOfWaves = 4;
+    _amplitude = 0.15;
+    _waveWidth = SELF_WIDTH;
+    _waveMid = _waveWidth / 2;
+    _density = 1.0;
+    _maxAmplitude = 90;
+    _frequency = 1.2f;
+    _phaseShift = -0.25f;
+    _idleAmplitude = 0.01f;
+    
+    _waveLineLayer = [CAShapeLayer layer];
+    _waveLineLayer.frame = self.bounds;
+    _waveLineLayer.lineCap       = kCALineCapButt; //指定线的边缘
+    _waveLineLayer.lineJoin      = kCALineJoinRound;
+    _waveLineLayer.fillColor     = [[UIColor clearColor] CGColor]; //波纹的填充色
+    [_waveLineLayer setLineWidth:3];
+    _waveLineLayer.strokeColor = [STLoopProgressView centerColor].CGColor; //指定path的渲染颜色
+//    switch (i) {
+//        case 0:
+//            [waveline setLineWidth:3];
+//            waveline.strokeColor = [[UIColor colorFromHexString:@"#fcc080"] CGColor]; //指定path的渲染颜色
+//            break;
+//        case 1:
+//            [waveline setLineWidth:2.5];
+//            waveline.strokeColor = [[UIColor colorFromHexString:@"#ffb8b6" alpha:0.4] CGColor];
+//            break;
+//        case 2:
+//            [waveline setLineWidth:2];
+//            waveline.strokeColor = [[UIColor colorFromHexString:@"#fcc080"] CGColor];
+//            break;
+//        case 3:
+//            [waveline setLineWidth:1.5];
+//            waveline.strokeColor = [[UIColor colorFromHexString:@"#ffb8b6" alpha:0.4] CGColor];
+//            break;
+//        default:
+//            break;
+//    }
+//    [self.layer addSublayer:_waveLineLayer];
+    self.colorLayer.mask = _waveLineLayer;
+//    self.layer.mask = _waveLineLayer;
 }
 
 /**
  *  设置整个蓝色view的遮罩
  */
 - (void)setupBlueMaskLayer {
-    
     CAShapeLayer *layer = [self generateMaskLayer];
     self.layer.mask = layer;
     self.blueMaskLayer = layer;
@@ -47,7 +105,6 @@
  *  设置渐变色，渐变色由左右两个部分组成，左边部分由黄到绿，右边部分由黄到红
  */
 - (void)setupColorLayer {
-    
     self.colorLayer = [CAShapeLayer layer];
     self.colorLayer.frame = self.bounds;
     [self.layer addSublayer:self.colorLayer];
@@ -70,7 +127,6 @@
  *  设置渐变色的遮罩
  */
 - (void)setupColorMaskLayer {
-    
     CAShapeLayer *layer = [self generateMaskLayer];
     layer.lineWidth = [STLoopProgressView lineWidth] + 0.5; // 渐变遮罩线宽较大，防止蓝色遮罩有边露出来
     self.colorLayer.mask = layer;
@@ -84,7 +140,6 @@
  *  @return 环形遮罩
  */
 - (CAShapeLayer *)generateMaskLayer {
-    
     CAShapeLayer *layer = [CAShapeLayer layer];
     layer.frame = self.bounds;
     
@@ -111,9 +166,58 @@
  *  @param persentage 百分比
  */
 - (void)setPersentage:(CGFloat)persentage {
-    
     _persentage = persentage;
     self.colorMaskLayer.strokeEnd = persentage;
+    
+    [self updateMaveLevel];
+}
+
+- (void)updateMaveLevel {
+    _persentage = 0.5;
+    self.phase += self.phaseShift; // Move the wave
+    self.amplitude = fmax(_persentage, self.idleAmplitude);
+    
+    UIGraphicsBeginImageContext(self.frame.size);
+    UIBezierPath *wavelinePath = [UIBezierPath bezierPath];
+    
+    // Progress is a value between 1.0 and -0.5, determined by the current wave idx, which is used to alter the wave's amplitude.
+    
+    int i = 0;
+    CGFloat progress = 1.0f - (CGFloat)i / self.numberOfWaves;
+    CGFloat normedAmplitude = (1.5f * progress - 0.5f) * self.amplitude;
+    //        NSLog(@"progress:%f, self.amplitude:%f, normedAmplitude:%f", progress, self.amplitude, normedAmplitude);
+    CAShapeLayer *waveline = _waveLineLayer;
+    
+    //x初始值依赖于self.frame.origin.x
+    for (CGFloat x = self.frame.origin.x; x < self.waveWidth + self.density; x += self.density) {
+        //Thanks to https://github.com/stefanceriu/SCSiriWaveformView
+        // We use a parable to scale the sinus wave, that has its peak in the middle of the view.
+        //缩放
+        //double pow (double base, double exponent);求base的exponent次方值
+        if (self.waveMid == 0) {
+            continue;
+        }
+        CGFloat scaling = -pow(x / self.waveMid  - 1, 2) + 1; // make center bigger
+        //sinf：计算正弦值和双曲线的正弦值
+        CGFloat y = scaling * self.maxAmplitude * normedAmplitude * sinf(2 * M_PI *(x / self.waveWidth) * self.frequency + self.phase) + (self.waveHeight * 0.5);
+        
+        if (x == self.frame.origin.x) {
+            /**
+             *  设置第一个起始点到接收器
+             *  @param point 起点坐标
+             */
+            [wavelinePath moveToPoint:CGPointMake(x, y)];
+        } else {
+            /**
+             *  附加一条直线到接收器的路径
+             *  @param point 要到达的坐标
+             */
+            [wavelinePath addLineToPoint:CGPointMake(x, y)];
+        }
+        waveline.path = [wavelinePath CGPath];
+    }
+    
+    UIGraphicsEndImageContext();
 }
 
 @end
